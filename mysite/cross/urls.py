42801f453741544e9f9f3b371917ff556b6129a2
kndt84 <takashi.kaneda@vasily.jp>
2015-08-30 07:01:10 +0900

    article detail 追加

--- mysite/cross/urls.py
+++ mysite/cross/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^get_articles/(?P<scene>.+)$', views.get_articles, name='get_articles'),
+    url(r'^get_articles/(?P<keyword>.+)$', views.get_articles, name='get_articles'),
+    url(r'^article_detail/(?P<keyword>.+)/(?P<id>[0-9]+)$', views.article_detail, name='les'),
     url(r'^get_items/(?P<scene>.+)$', views.get_articles, name='get_items'),
 ]
--- mysite/cross/views.py
+++ mysite/cross/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
 import urllib
 import urllib2
 import json
+
 
 # 
 password_mgr = urllib2.HTTPPasswordMgrWithDefaultRealm()
@@ -13,20 +16,39 @@ handler = urllib2.HTTPBasicAuthHandler(password_mgr)
 opener = urllib2.build_opener(handler)
 urllib2.install_opener(opener)
 
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 
-def get_articles(request, scene):
+def get_articles(request, keyword):
     
-    f = json.load(urllib2.urlopen("http://fashionhack.jp/api/search.php?keyword="+scene.encode('utf-8')))
-#    obs = json.dumps(f.read(), ensure_ascii=False, indent=4)
-#    obs = json.loads(f.read())
-
+    f = json.load(urllib2.urlopen("http://fashionhack.jp/api/search.php?keyword="+keyword.encode('utf-8')))
     image_urls = []
     for i in range(0,len(f)):
         image_urls.append(f['matches'][i]['document']['fields']['thumbnail'])
 
+    context = {'image_urls' : image_urls}
+    return render(request, 'cross/get_articles.html', context)
 
-    return HttpResponse(image_urls)
-#    return HttpResponse(f['matches'][1]['document']['fields']['images'])
+#    return HttpResponse(image_urls)
+
+
+
+def article_detail(request, keyword, id):
+    
+    f = json.load(urllib2.urlopen("http://fashionhack.jp/api/search.php?keyword="+keyword.encode('utf-8')))
+
+
+    id = int(id)
+    
+    dic = {}
+    dic['image_url'] = f['matches'][id]['document']['fields']['thumbnail']
+    dic['magazinename'] = f['matches'][id]['document']['fields']['magazinename']
+    dic['month'] = f['matches'][id]['document']['fields']['month']
+    dic['publisher'] = f['matches'][id]['document']['fields']['publisher']
+    dic['text'] = f['matches'][id]['document']['fields']['text_shingle2']
+
+    context = {'article_info' : dic}
+    return render(request, 'cross/article_detail.html', context)
+
